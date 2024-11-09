4a25b5ce440a8ca0a0e7646bc2a210d1b9e9559d
Luke Wyman <c-luke.wyman@charter.com>
2024-11-08 18:44:47 -0800

    Added all vpc outputs for outputs_lookup

--- outputs_lookup/outputs.tf
+++ outputs_lookup/outputs.tf
@@ -1,3 +1,36 @@
+output "vpc_azs" {
+  value = data.terraform_remote_state.vpc.outputs.azs
+}
+
+output "vpc_database_subnets" {
+  value = data.terraform_remote_state.vpc.outputs.database_subnets
+}
+
+output "vpc_nat_public_ips" {
+  value = data.terraform_remote_state.vpc.outputs.nat_public_ips
+}
+
+output "vpc_private_subnets" {
+  value = data.terraform_remote_state.vpc.outputs.private_subnets
+}
+
+output "vpc_private_subnets_cidr_blocks" {
+  value = data.terraform_remote_state.vpc.outputs.private_subnets_cidr_blocks
+}
+
+output "vpc_public_subnets" {
+  value = data.terraform_remote_state.vpc.outputs.public_subnets
+}
+
+output "vpc_public_subnets_cidr_blocks" {
+  value = data.terraform_remote_state.vpc.outputs.public_subnets_cidr_blocks
+}
+
+output "vpc_vpc_cidr_block" {
+  value = data.terraform_remote_state.vpc.outputs.vpc_cidr_block
+}
+
 output "vpc_vpc_id" {
   value = data.terraform_remote_state.vpc.outputs.vpc_id
 }
+
