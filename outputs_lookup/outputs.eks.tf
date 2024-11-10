c81d07ecc28e90ed83422fcdb33566672c968e7b
Luke Wyman <c-luke.wyman@charter.com>
2024-11-10 15:08:41 -0800

    Removed eks_ and vpc_ prefixes from outputs

--- outputs_lookup/outputs.eks.tf
+++ outputs_lookup/outputs.eks.tf
@@ -1,15 +1,15 @@
-output "eks_aws_iam_openid_connect_provider_arn" {
+output "aws_iam_openid_connect_provider_arn" {
   value = data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_arn
 }
 
-output "eks_cluster_certificate_authority_data" {
+output "cluster_certificate_authority_data" {
   value = data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data
 }
 
-output "eks_eks_cluster_id" {
+output "eks_cluster_id" {
   value = data.terraform_remote_state.eks.outputs.eks_cluster_id
 }
 
-output "eks_eks_cluster_endpoint" {
+output "eks_cluster_endpoint" {
   value = data.terraform_remote_state.eks.outputs.eks_cluster_endpoint
 }
--- outputs_lookup/outputs.vpc.tf
+++ outputs_lookup/outputs.vpc.tf
@@ -1,36 +1,36 @@
-output "vpc_azs" {
+output "azs" {
   value = data.terraform_remote_state.vpc.outputs.azs
 }
 
-output "vpc_database_subnets" {
+output "database_subnets" {
   value = data.terraform_remote_state.vpc.outputs.database_subnets
 }
 
-output "vpc_nat_public_ips" {
+output "nat_public_ips" {
   value = data.terraform_remote_state.vpc.outputs.nat_public_ips
 }
 
-output "vpc_private_subnets" {
+output "private_subnets" {
   value = data.terraform_remote_state.vpc.outputs.private_subnets
 }
 
-output "vpc_private_subnets_cidr_blocks" {
+output "private_subnets_cidr_blocks" {
   value = data.terraform_remote_state.vpc.outputs.private_subnets_cidr_blocks
 }
 
-output "vpc_public_subnets" {
+output "public_subnets" {
   value = data.terraform_remote_state.vpc.outputs.public_subnets
 }
 
-output "vpc_public_subnets_cidr_blocks" {
+output "public_subnets_cidr_blocks" {
   value = data.terraform_remote_state.vpc.outputs.public_subnets_cidr_blocks
 }
 
-output "vpc_vpc_cidr_block" {
+output "vpc_cidr_block" {
   value = data.terraform_remote_state.vpc.outputs.vpc_cidr_block
 }
 
-output "vpc_vpc_id" {
+output "vpc_id" {
   value = data.terraform_remote_state.vpc.outputs.vpc_id
 }
 
