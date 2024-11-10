f519efaea7e44b9fadf5b34eefa4d59a950fbfa3
Luke Wyman <c-luke.wyman@charter.com>
2024-11-10 14:55:35 -0800

    Refactored outputs for outputs_lookup

--- outputs_lookup/data.tf
+++ outputs_lookup/data.tf
@@ -7,3 +7,13 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+data "terraform_remote_state" "eks" {
+  backend = "s3"
+
+  config = {
+    bucket = "karaoke-infrastructure-state"
+    key = "infrastructure/eks_cluster/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
